df3074a0358f4f4adf69f12f5b58c7818d81f041
Richard Harris <richard@idestini.com>
2020-03-09 22:16:04 -0500

    Added body-parser, wildcard

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 //const coors = require('cors');
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-//app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(cors);
 
 app.get('/api/chicken', (req, res) => {
-    res.send("Cluck cluck");
+    res.send("Bawk bawk");
 })
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client/build/index.html'));
-// })
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
